0e862211b129f77f1ba0e55624624fbcb0e45566
Ricardo Franco <ricardofranco2509@gmail.com>
2023-08-18 17:13:50 -0300

    populando tabelas

--- api/seeders/20230818190957-demo-nivel.js
+++ api/seeders/20230818190957-demo-nivel.js
@@ -2,24 +2,27 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Niveis', [
+      {
+        descr_nivel: 'básico',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        descr_nivel: 'intermediário',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        descr_nivel: 'avançado',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Niveis', null, {});
   }
 };
--- api/seeders/20230818191019-demo-matriculas.js
+++ api/seeders/20230818191019-demo-matriculas.js
@@ -2,24 +2,55 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Matriculas', [
+      {
+        status: "confirmado",
+        estudante_id: 1,
+        turma_id: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        status: "confirmado",
+        estudante_id: 2,
+        turma_id: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        status: "confirmado",
+        estudante_id: 3,
+        turma_id: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        status: "confirmado",
+        estudante_id: 4,
+        turma_id: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        status: "cancelado",
+        estudante_id: 1,
+        turma_id: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        status: "cancelado",
+        estudante_id: 2,
+        turma_id: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Matriculas', null, {});
   }
 };
